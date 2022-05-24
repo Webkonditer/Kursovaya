a75a635c4b52ef182b999a8ef09dad11f820bb6f
Vilas <webkonditer@yandex.ru>
2022-05-24 15:41:49 +0700

    Повышенная сложность

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,15 @@ public class Main {
         System.out.println("Сотрудник с максимальной зарплатой: " + employees[maxSalary(employees)]);
         System.out.println("Средняя зарплата: " + averageSalary(employees));
         fullNames(employees);
+        indexingSalary(employees, -10);
+        minDepartmentSalary(employees, 2);
+        maxDepartmentSalary(employees, 2);
+        allDepartmentSalary(employees, 2);
+        averageDepartmentSalary(employees, 2);
+        indexingDepartmentSalary( employees, 2, 10);
+        departmentList(employees, 2);
+        salaryLessThen(employees, 30000);
+        salaryMoreThen(employees, 30000);
     }
 
     public static void fullList(Employee[] employees) {
@@ -80,6 +89,108 @@ public class Main {
         for (int i = 0; i < employees.length; i++) {
             if(employees[i] != null){
                 System.out.println(employees[i].getFullName());
+            }
+        }
+    }
+
+    public static void indexingSalary(Employee[] employees, int percent) {
+        for (int i = 0; i < employees.length; i++) {
+            if(employees[i] != null){
+                int salary = employees[i].getSalary();
+                employees[i].setSalary(salary + salary / 100 * percent );
+            }
+        }
+    }
+
+    public static void minDepartmentSalary(Employee[] employees, int department) {
+        int min = 500000;
+        int counter = 0;
+
+        for (int i = 0; i < employees.length; i++) {
+            if(employees[i] != null && employees[i].getDepartment() == department && min > employees[i].getSalary()){
+                min = employees[i].getSalary();
+                counter = i;
+            }
+        }
+        System.out.println("Сотрудник с минимальной зарплатой в отделе " + department + ": " + employees[counter]);
+    }
+
+    public static void maxDepartmentSalary(Employee[] employees, int department) {
+        int max = 0;
+        int counter = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if(employees[i] != null && employees[i].getDepartment() == department && max < employees[i].getSalary()){
+                max = employees[i].getSalary();
+                counter = i;
+            }
+        }
+        System.out.println("Сотрудник с максимальной зарплатой в отделе " + department + ": " + employees[counter]);
+    }
+
+    public static void allDepartmentSalary(Employee[] employees, int department) {
+        int summ = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if(employees[i] != null && employees[i].getDepartment() == department){
+                summ = summ + employees[i].getSalary();
+            }
+        }
+        System.out.println("Сумма затрат на зарплату по отделу " + department + ": " + summ);
+    }
+
+    public static void averageDepartmentSalary(Employee[] employees, int department) {
+        int summ = 0;
+        int counter = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if(employees[i] != null && employees[i].getDepartment() == department){
+                summ = summ + employees[i].getSalary();
+                counter++;
+            }
+        }
+        System.out.println("Средняя зарплата по отделу " + department + ": " + (summ / counter));
+    }
+
+    public static void indexingDepartmentSalary(Employee[] employees, int department, int percent) {
+        for (int i = 0; i < employees.length; i++) {
+            if(employees[i] != null && employees[i].getDepartment() == department){
+                int salary = employees[i].getSalary();
+                employees[i].setSalary(salary + salary / 100 * percent );
+            }
+        }
+    }
+
+    public static void departmentList(Employee[] employees, int department) {
+        System.out.println("Список сотрудников отдела " + department + ":");
+        for (int i = 0; i < employees.length; i++) {
+            if(employees[i] != null && employees[i].getDepartment() == department){
+                System.out.println("id=" + employees[i].getId() +
+                        ", fullName='" + employees[i].getFullName() + '\'' +
+                        ", department=" + employees[i].getDepartment() +
+                        ", salary=" + employees[i].getSalary()
+                );
+            }
+        }
+    }
+
+    public static void salaryLessThen(Employee[] employees, int summ) {
+        System.out.println("Список сотрудников с зарплатой менее " + summ + ":");
+        for (int i = 0; i < employees.length; i++) {
+            if(employees[i] != null && employees[i].getSalary() < summ){
+                System.out.println("id=" + employees[i].getId() +
+                        ", fullName='" + employees[i].getFullName() + '\'' +
+                        ", salary=" + employees[i].getSalary()
+                );
+            }
+        }
+    }
+
+    public static void salaryMoreThen(Employee[] employees, int summ) {
+        System.out.println("Список сотрудников с зарплатой более или равной " + summ + ":");
+        for (int i = 0; i < employees.length; i++) {
+            if(employees[i] != null && employees[i].getSalary() >= summ){
+                System.out.println("id=" + employees[i].getId() +
+                        ", fullName='" + employees[i].getFullName() + '\'' +
+                        ", salary=" + employees[i].getSalary()
+                );
             }
         }
     }
